7fe76b8af05aa21ac9490df46271dd5d5399ea37
Xiao Lu <frankluxiao@gmail.com>
2020-08-16 21:46:50 -0700

    First Commit

---
+++ src/main/java/com/example/mavenspringbootkafka/MavenSpringBootKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenspringbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSpringBootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenSpringBootKafkaApplication.class, args);
+	}
+
+}
